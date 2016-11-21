ffc215913b750e5fc906564f7623aebbc37707cf
Jenna Smith <jenna@growldesign.co.uk>
2016-11-21 13:51:32 +0000

    DRY up utils

--- plugins/utils.js
+++ plugins/utils.js
@@ -1,26 +1,27 @@
 var _ = require('lodash-fp');
 var moment = require('moment');
 
+var sortByDate = _.sortByOrder(_.get('date'));
+var sortByDateDesc = sortByDate(false);
+var sortByDateAsc = sortByDate(true);
+var isPeriod = _.curry(function (operator, fn, item) {
+  return operator(0, fn(item.metadata.date));
+});
+
+function diff(date) {
+  var now = moment();
+  var date = moment(date);
+  return date.diff(now);
+}
+
 function past(items) {
-  var sorted = _.sortByOrder(_.get('date'), false);
-
-  return sorted(items).filter(function (item) {
-    var now = moment();
-    var date = moment(item.metadata.date);
-
-    return date.diff(now) < 0;
-  });
+  var isPast = isPeriod(_.lt);
+  return sortByDateDesc(items).filter(isPast(diff));
 };
 
 function present(items) {
-  var sorted = _.sortByOrder(_.get('date'), true);
-
-  return sorted(items).filter(function (item) {
-    var now = moment();
-    var date = moment(item.metadata.date);
-
-    return date.diff(now) >= 0;
-  });
+  var isPresent = isPeriod(_.gte);
+  return sortByDateAsc(items).filter(isPresent(diff));
 }
 
 module.exports = (env, callback) => {
