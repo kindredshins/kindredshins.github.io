df6729202c33d704293961b0be10fd3f18e14e02
Jenna Smith <jenna@growldesign.co.uk>
2016-12-02 20:35:21 +0000

    Move background JS into background component folder

---
+++ src/components/background/background.js
@@ -0,0 +1,5 @@
+var FrameVideo = require('./../../scripts/frame-video');
+var where = document.querySelector('#ks-background');
+var video = new FrameVideo(where, 24);
+
+video.init('/assets/videos/video.json');
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,2 @@
-var svgxuse = require('svgxuse');
-var FrameVideo = require('./frame-video');
-var where = document.querySelector('#ks-background');
-var video = new FrameVideo(where, 24);
-
-video.init('/assets/videos/video.json');
+require('svgxuse');
+require('./../components/background/background');
