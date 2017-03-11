730c1388d9e630b630cd82021cbf9e8a15dfb120
Jenna Smith <jenna@growldesign.co.uk>
2017-03-11 18:57:42 +0000

    Style photos

--- src/components/photos/photos.js
+++ src/components/photos/photos.js
@@ -9,18 +9,23 @@ var instagram = require('./../../shared/instagram');
     var fragment = window.document.createDocumentFragment();
 
     photos.forEach(function(photo) {
-      var item = window.document.createElement('li');
+      var item = window.document.createElement('article');
       var link = window.document.createElement('a');
       var img = window.document.createElement('img');
 
       link.href = photo.url;
       link.target = '_blank';
       img.src = photo.src;
+      item.classList.add('ks-photos__item');
 
       link.appendChild(img);
       item.appendChild(link);
       fragment.appendChild(item);
     });
+
+    while (context.hasChildNodes()) {
+        context.removeChild(context.lastChild);
+    }
 
     context.appendChild(fragment);
   }
